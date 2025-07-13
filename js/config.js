7a25a1989f4fabb47ff78a195f858ea170e981b4
sidath20241866 <sidath.20241866@iit.ac.lk>
2025-07-13 17:24:31 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://d1pj8avzs1.execute-api.us-east-1.amazonaws.com/dev2' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://d1pj8avzs1.execute-api.us-east-1.amazonaws.com/dev3' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
